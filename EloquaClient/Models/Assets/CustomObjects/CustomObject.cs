4b2275eae42bbe18cd829c8bbe47207e32b1631d
Kees VanTilburg <kees.vantilburg@live.com>
2016-09-16 17:12:58 -0500

    add properties to custom objects

--- EloquaClient/Models/Assets/CustomObjects/CustomObject.cs
+++ EloquaClient/Models/Assets/CustomObjects/CustomObject.cs
@@ -5,9 +5,18 @@ namespace Eloqua.Api.Rest.ClientLibrary.Models.Assets.CustomObjects
     [Resource("/assets/customObject", "CustomObject")]
     public class CustomObject : RestObject, ISearchable
     {
-        public string displayNameFieldId { get; set; }
+        public string description { get; set; }
+        public string currentStatus { get; set; }
+        public int recordCount { get; set; }
         public List<CustomObjectField> fields { get; set; }
+        public string displayNameFieldId { get; set; }
         public string uniqueCodeFieldId { get; set; }
+        public string emailAddressFieldId { get; set; }
+        public string folderId { get; set; }
+        public string createdBy { get; set; }
+        public string createdAt { get; set; }
+        public string updatedBy { get; set; }
+        public string updatedAt { get; set; }
 
         #region ISearchable
 
--- EloquaClient/Models/Assets/CustomObjects/CustomObjectField.cs
+++ EloquaClient/Models/Assets/CustomObjects/CustomObjectField.cs
@@ -1,13 +1,24 @@
 ﻿namespace Eloqua.Api.Rest.ClientLibrary.Models.Assets.CustomObjects
 {
-    public class CustomObjectField
+    public class CustomObjectField 
     {
         public int? id { get; set; }
         public string name { get; set; }
+
+        public string type;
+        public string depth { get; set; }
+        public string description { get; set; }
+        public string currentStatus { get; set; }
         public string dataType { get; set; }
         public string defaultValue { get; set; }
         public string displayType { get; set; }
         public string internalName { get; set; }
-        public string type { get; set; }
+        public string optionListId { get; set; }
+        public string uncheckedValue { get; set; }
+        public string folderId { get; set; }
+        public string createdBy { get; set; }
+        public string createdAt { get; set; }
+        public string updatedBy { get; set; }
+        public string updatedAt { get; set; }
     }
 }
