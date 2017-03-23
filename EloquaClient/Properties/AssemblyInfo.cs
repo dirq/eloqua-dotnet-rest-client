eb512f5cbb57694c51b0cec66830262b7b5230d1
Kees VanTilburg <kees.vantilburg@live.com>
2017-03-23 15:33:08 -0500

    Package and reference updates

--- EloquaClient/Properties/AssemblyInfo.cs
+++ EloquaClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EloquaClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("EloquaRequest")]
+[assembly: AssemblyDescription("Forked from Eloqua.Api.Rest.ClientLibrary by Fred Sakr")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Eloqua.Api.Rest.ClientLibrary")]
-[assembly: AssemblyCopyright("Fred Sakr")]
+[assembly: AssemblyCompany("Intelechy")]
+[assembly: AssemblyProduct("EloquaRequest")]
+[assembly: AssemblyCopyright("Copyright © Intelechy 2016-17")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
