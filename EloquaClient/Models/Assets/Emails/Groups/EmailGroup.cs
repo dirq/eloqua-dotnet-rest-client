1659957f3d54c65e4466b2c8e3bb74b73f13279e
Kees VanTilburg <kees.vantilburg@live.com>
2017-04-10 16:13:50 -0500

    Added email groups

--- EloquaClient/Models/Assets/Emails/Groups/EmailGroup.cs
+++ EloquaClient/Models/Assets/Emails/Groups/EmailGroup.cs
@@ -6,19 +6,28 @@ namespace Eloqua.Api.Rest.ClientLibrary.Models.Assets.Emails.Groups
     public class EmailGroup : RestObject, ISearchable
     {
         public string displayName { get; set; }
-        public int? emailFooterId { get; set; }
-        public int? emailHeaderId { get; set; }
+        public long? emailFooterId { get; set; }
+        public long? emailHeaderId { get; set; }
         public List<string> emailIds { get; set; }
         public bool? isVisibleInOutlookPlugin { get; set; }
         public string subscriptionListDataLookupId { get; set; }
-        public int? subscriptionLandingPageId { get; set; }
-        public int? subscriptionListId { get; set; }
+        public long? subscriptionLandingPageId { get; set; }
+        public long? subscriptionListId { get; set; }
         public string unSubscriptionListDataLookupId { get; set; }
-        public int? unsubscriptionLandingPageId { get; set; }
-        public int? unSubscriptionListId { get; set; }
+        public long? unsubscriptionLandingPageId { get; set; }
+        public long? unSubscriptionListId { get; set; }
         public int page { get; set; }
         public int pageSize { get; set; }
         public string searchTerm { get; set; }
         public string orderByField { get; set; }
+        public bool? isVisibleInPublicSubscriptionList { get; set; }
+        public bool? requireOptIn { get; set; }
+        public bool? useSecureChannel { get; set; }
+        public string currentStatus { get; set; }
+        public int? accessedAt { get; set; }
+        public int? createdAt { get; set; }
+        public int? createdBy { get; set; }
+        public int? updatedAt { get; set; }
+        public int? updatedBy { get; set; }
     }
 }
